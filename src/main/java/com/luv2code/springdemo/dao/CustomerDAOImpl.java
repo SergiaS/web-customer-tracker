fc7925d8f4761e60ce75ae36a0af8fe7c54e4670
sergias <sergias@ukr.net>
2020-07-13 17:57:07 +0300

    creating DAO layer

---
+++ src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,39 @@
+package com.luv2code.springdemo.dao;
+
+import com.luv2code.springdemo.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	// need to inject the hibernate session factory
+	// for DAO can use it for talking the db
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	// let Spring manage starting and stopping the transactions
+	@Transactional
+	public List<Customer> getCustomers() {
+
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// create a query
+		Query<Customer> theQuery =
+				currentSession.createQuery("from Customer", Customer.class);
+
+		// execute query and get result list
+		List<Customer> customers = theQuery.getResultList();
+
+		// return the results
+		return customers;
+	}
+}
