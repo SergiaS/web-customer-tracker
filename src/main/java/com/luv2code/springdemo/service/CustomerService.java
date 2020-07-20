b0bb7d5d103633a3bab30eae49e0d91f508d80cc
sergias <sergias@ukr.net>
2020-07-20 15:12:23 +0300

    adding service layer

---
+++ src/main/java/com/luv2code/springdemo/service/CustomerService.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo.service;
+
+import com.luv2code.springdemo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+
+}
